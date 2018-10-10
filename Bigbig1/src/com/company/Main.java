a591a154ec90849115ee512267cd653b4da10760
Nguyen Chi Dat <40425312+ChiDatNguyen@users.noreply.github.com>
2018-10-10 13:59:56 +0700

    Commandline version
    thứ Tư : 10/10/2018

---
+++ Bigbig1/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        DictionaryCommandline dictionaryCommandline = new DictionaryCommandline();
+
+
+//      dictionaryCommandline.dictionaryBasic();
+
+        dictionaryCommandline.dictionaryAdvance();
+        dictionaryCommandline.dictionarySeacher();
+    }
+}
